f4e056c6871d20e322251a0db8e296ab6b911089
xuhongyi <xuhongyi@exponech.com>
2024-03-26 20:36:43 +0800

    feat: 添加cookie接受条款 xuhongyi

---
+++ app/components/CookieAccept.tsx
@@ -0,0 +1,8 @@
+import { useTranslation } from "../i18n"
+export default async function CookieAccept({ lng }: any) {
+    const { t } = await useTranslation(lng, 'cookie')
+
+    return <div>
+
+    </div>
+}
